14ce7870d954d8243795dbd99bcc45984728a52e
vlad antipenko <vlad@force-up.com>
2017-10-16 18:01:51 +0300

    add layout for project

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,25 @@
 import css from '../css/style.scss';
+import $ from 'jquery';
+import slick from 'slick-carousel';
 
 function app() {
-
-
+    "use strict";
+    $(document).ready(function () {
+        $('.announces-slider').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            responsive: [
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    });
 
 }
 
