e3e39450be0cdd310f0de69405ae5defc21b83f7
vlad antipenko <vlad@force-up.com>
2017-10-17 10:29:22 +0300

    fixed h2 and added news

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import slick from 'slick-carousel';
 function app() {
     "use strict";
     $(document).ready(function () {
+        //announce slider on home page
         $('.announces-slider').slick({
             infinite: true,
             slidesToShow: 3,
@@ -19,8 +20,30 @@ function app() {
                 }
             ]
         });
+        // news slider on home page
+        $('.news-slider').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            responsive: [
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
     });
-
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 300) {
+            $('.vb-header').addClass('sticky');
+        }
+        else {
+            $('.vb-header').removeClass('sticky');
+        }
+    });
 }
 
 window.addEventListener('load', app);
